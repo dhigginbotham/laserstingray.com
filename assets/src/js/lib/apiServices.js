1676a8472cfedb2084cc512007e35c3e700e8e0e
David Higginbotham <davehigginbotham@gmail.com>
2015-03-13 00:08:07 -0600

    cleaned up error handling

--- assets/src/js/lib/apiServices.js
+++ assets/src/js/lib/apiServices.js
@@ -1,13 +1,12 @@
 var apiServices = (function(w, d, $, pub) {
 
-  var conf, crud, init, methods, xhr;
-
-  methods = ['get', 'post', 'put', 'delete'];
+  var conf, crud, init, xhr;
 
   conf = {
     auth: false, // should be an object if used
     baseUrl: '/api',
-    exposeXhr: true
+    exposeXhr: true,
+    methods: ['get', 'post', 'put', 'delete']
   };
 
   crud = function(method) {
@@ -17,6 +16,9 @@ var apiServices = (function(w, d, $, pub) {
         opts = null;
       }
       var id = (req.id ? req.id : null);
+      if ((method === 'put' || method === 'delete') && !id) {
+        return fn({fatal: 'Expected an id for method type: ' + method}, null);
+      }
       if (id) delete req.id;
       var request = {
         url: '/' + collection + (id ? '/' + id : ''),
@@ -28,10 +30,11 @@ var apiServices = (function(w, d, $, pub) {
   };
 
   init = function(fn) {
+    var copy = Array.prototype.slice.call(conf.methods);
     var processor = function() {
-      var cur = methods.shift();
+      var cur = copy.shift();
       pub[cur] = crud(cur);
-      if (methods.length) return processor();
+      if (copy.length) processor();
     };
     processor();
     return fn;
@@ -41,10 +44,20 @@ var apiServices = (function(w, d, $, pub) {
   xhr = function(req, opts, fn) {
     var callback, promise, operator, options;
 
+    if (typeof req === 'undefined') return false;
+
     if (typeof opts === 'function' && typeof fn === 'undefined') {
       fn = opts;
       opts = null;
     }
+
+    callback = function(data, status) {
+      if (status == 'success') {
+        return fn(null, data);
+      } else {
+        return fn({error: data.responseText, status: data.status}, null);
+      }
+    };
 
     operator = (!!~req.url.indexOf('?') ? '&' : '?');
     options = {
@@ -54,16 +67,13 @@ var apiServices = (function(w, d, $, pub) {
 
     if (opts) $.extend(options, opts);
 
-    callback = function(data, status) {
-      if (status == 'success') {
-        return fn(null, data);
-      } else {
-        return fn({msg: data.responseText, status: data.status}, null);
-      }
-    };
+    // add baseUrl to api request uri
+    req.url = conf.baseUrl + req.url;
 
-    // add baseUrl to api request
-    req.url = conf.baseUrl + req.url + (options.cacheBusting ? operator + options.cacheBusting + '=' + new Date().getTime() : '');
+    // add cacheBusting to api request uri
+    if (options.cacheBusting) {
+      req.url += operator + options.cacheBusting + '=' + new Date().getTime();
+    }
 
     promise = $.ajax(req);
 
@@ -74,7 +84,11 @@ var apiServices = (function(w, d, $, pub) {
     return promise;
   };
 
-  return init(function(cfg, mixins) {
+  pub.cfg = function() {
+    return conf;
+  };
+
+  return init(function(cfg) {
     if (cfg) $.extend(conf, cfg);
     if (conf.exposeXhr) pub.xhr = xhr;
     return pub;
