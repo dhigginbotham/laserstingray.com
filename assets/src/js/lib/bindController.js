029d9d2845a9f2b5a076c7f5fee82a8650886ce2
David Higginbotham <davehigginbotham@gmail.com>
2015-03-19 22:36:31 -0600

    added a couple more bind enhancements

--- assets/src/js/lib/bindController.js
+++ assets/src/js/lib/bindController.js
@@ -1,9 +1,12 @@
 var bindController = (function(w,d,$,pub) {
-  var model, state;
+  
+  var model, ready, state, subscribers;
   var api, bindElements, collectState, init;
 
   model = {};
-  pub.bound = false;
+  pub.binding = false;
+  ready = false;
+  subscribers = {};
 
   api = function(context) {
     if (typeof context != 'undefined') $.extend(model, context);
@@ -12,37 +15,42 @@ var bindController = (function(w,d,$,pub) {
   };
 
   pub.bind = bindElements = function() {
-    for (var i=0;i<state.length;++i) {
-      var val = model[state[i].getAttribute('data-bind')];
-      if (val) {
-        if (!state[i].getAttribute('data-bound')) state[i].setAttribute('data-bound', true);
-        state[i].innerText = val;
-      } else {
-        continue;
-      }
-    }
-  };
-
-  pub.update = collectState = function(el) {
-    pub.bound = true;
-    state = (el ? el.getElementsByTagName('*') : d.getElementsByTagName('*'));
-    for (var i=0;i<state.length;++i) {
-      var key, found, val;
-      if (state[i].getAttribute('data-bound') === null) {
-        key = state[i].getAttribute('data-bind');
-        if (key !== null) {
-          val = (state[i].innerText ? state[i].innerText : state[i].getAttribute('data-val'));
-          if (val && !model[key]) {
-            model[key] = val;
-          } else {
-            model[key] = null;
+    if (ready) {
+      pub.binding = false;
+      for (var i=0;i<state.length;++i) {
+        var elem, val;
+        elem = state[i];
+        val = model[elem.getAttribute('data-bind')];
+        if (val) {
+          if (!elem.getAttribute('data-bound')) elem.setAttribute('data-bound', true);
+          if (elem.getAttribute('data-bind-clean') === null) {
+            elem.setAttribute('data-bind-clean', val);
           }
+          elem.setAttribute('data-bind-dirty', val);
+          elem.innerText = val;
         } else {
           continue;
         }
       }
     }
-    bindElements();
+  };
+
+  pub.update = collectState = function(el) {
+    pub.binding = true;
+    state = (typeof el !== 'undefined' ? el.getElementsByTagName('*') : d.getElementsByTagName('*'));
+    for (var i=0;i<state.length;++i) {
+      var key, found, val;
+      if (state[i].getAttribute('data-bound') === null) {
+        key = state[i].getAttribute('data-bind');
+        if (key !== null) {
+          val = (state[i].innerText ? state[i].innerText : state[i].getAttribute('data-bind-clean'));
+          model[key] = (val && !model[key] ? val : null);
+        } else {
+          continue;
+        }
+      }
+    }
+    $(d).ready(init);
   };
 
   pub.set = function(key, val) {
@@ -51,14 +59,13 @@ var bindController = (function(w,d,$,pub) {
   };
 
   init = function() {
-    if (!pub.bound) collectState();
+    ready = true;
+    bindElements();
     pub.model = model;
     pub.state = state;
   };
 
   collectState();
-
-  $(d).ready(init);
 
   return api;
 
