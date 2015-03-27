ec6b9afba708a9b9a349dd88e5cadf58b6ffce52
David Higginbotham <davehigginbotham@gmail.com>
2015-03-26 21:46:12 -0600

    cleaning up some things and adding in simple editing

--- assets/src/js/lib/bindController.js
+++ assets/src/js/lib/bindController.js
@@ -1,13 +1,41 @@
 var bindController = (function(w,d,$,pub) {
   
+  /**
+   * bind controller allows you to do easy data
+   * binding from either your template layer or
+   * added in later as an xhr response or similar
+   *
+   * example usage:
+   * 1) <div data-bind="key" data-bind-first="some value"></div>
+   *
+   * var bc = bindController();
+   * bc.model ... {key: 'some value'};
+   *
+   * ----
+   *
+   * 2) <div data-bind="key">some value</div>
+   * 
+   * var bc = bindController();
+   * bc.model ... {key: 'some value'};
+   *
+   * ----
+   * 
+   */
+
   var model, ready, state;
   var api, bindElements, collectState, get, init, set;
 
+  // helper dependencies
   get = helperUtils.get;
   set = helperUtils.set;
 
+  // model, where our data will stay
   model = {};
+
+  // interally keep track of dom ready state
   ready = false;
+
+  // dom nodes we're binding on
   state = [];
 
   api = function(context) {
@@ -16,46 +44,44 @@ var bindController = (function(w,d,$,pub) {
     return pub;
   };
 
-  pub.bindElements = bindElements = function(key) {
-    if (ready) {
-      for (var i=0;i<state.length;++i) {
-        var count, elem, dirty, val, k;
-        elem = state[i];
-        k = (key ? key : elem.getAttribute('data-bind'));
-        val = (!~k.indexOf('.') ? model[k] : get(k, model));
-        dirty = (elem.type ? elem.getAttribute('value') : elem.innerHTML);
-        if (dirty && dirty == val) continue;
-        if (k == elem.getAttribute('data-bind') && typeof val != 'undefined') {
-          if (!elem.getAttribute('data-bound')) elem.setAttribute('data-bound', true);
-          if (val && elem.getAttribute('data-bind-first') === null) elem.setAttribute('data-bind-first', encodeURIComponent(val));
-          if (val && elem.getAttribute('data-bind-last') === null) elem.setAttribute('data-bind-last', val);
-          if (dirty && elem.getAttribute('data-bind-last') != dirty) elem.setAttribute('data-bind-last', dirty);
-          count = elem.getAttribute('data-bind-count');
-          if (count === null) {
-            elem.setAttribute('data-bind-count', 1);
-          } else {
-            elem.setAttribute('data-bind-count', parseFloat(count) + 1);
-          }
-          if (elem.type) {
-            elem.setAttribute('value', val);
-          } else {
-            elem.innerHTML = val;
-          }
+  /**
+   * bind elements from state/model to dom
+   * @param  {String} key optional, if not present will rebind all nodes
+   */
+  pub.bindElements = bindElements = function(k) {
+    if (!ready) return pub;
+    for (var i=0;i<state.length;++i) {
+      var count, elem, inner, val, key;
+      elem = state[i];
+      key = (k ? k : elem.getAttribute('data-bind'));
+      val = (!~key.indexOf('.') ? model[key] : get(key, model));
+      inner = (elem.type ? elem.getAttribute('value') : elem.innerHTML);
+      if (inner && inner == val) continue;
+      if (key == elem.getAttribute('data-bind') && typeof val != 'undefined') {
+        if (!elem.getAttribute('data-bound')) elem.setAttribute('data-bound', true);
+        if (val && elem.getAttribute('data-bind-first') === null) elem.setAttribute('data-bind-first', encodeURIComponent(val));
+        if (val && elem.getAttribute('data-bind-last') === null) elem.setAttribute('data-bind-last', val);
+        if (inner && elem.getAttribute('data-bind-last') != inner) elem.setAttribute('data-bind-last', inner);
+        count = elem.getAttribute('data-bind-count');
+        if (count === null) {
+          elem.setAttribute('data-bind-count', 1);
         } else {
-          continue;
+          elem.setAttribute('data-bind-count', parseFloat(count) + 1);
         }
+        if (elem.type) {
+          elem.setAttribute('value', val);
+        } else {
+          elem.innerHTML = val;
+        }
+      } else {
+        continue;
       }
     }
     return pub;
   };
 
-  pub.update = collectState = function(context, fn) {
+  pub.update = collectState = function(context) {
     var ctx, dom;
-    if (typeof context == 'function' && 
-        typeof fn == 'undefined') {
-      fn = context;
-      context = null;
-    }
     ctx = (context ? context : d);
     dom = ctx.getElementsByTagName('*');
     for (var i=0;i<dom.length;++i) {
@@ -75,7 +101,6 @@ var bindController = (function(w,d,$,pub) {
         continue;
       }
     }
-    if (typeof fn != 'undefined') fn();
     return pub;
   };
 
@@ -111,9 +136,9 @@ var bindController = (function(w,d,$,pub) {
     pub.state = state;
   };
 
-  collectState(function() {
-    $(d).ready(init);
-  });
+  collectState();
+  
+  $(d).ready(init);
 
   return api;
 
